7ca976dbb38b2768f441a7937d978e6f7cb4b31a
Louis Phang <design.vincent.louis@gmail.com>
2019-02-22 14:40:38 +0800

    correcting the caching method
    seems like i screw up, cause the firestore keep reloading the data from server.
    so now every update will have to manually caching into localstorage until a better solution.

--- src/services/useTodoData.js
+++ src/services/useTodoData.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { getTodoList, storeTodoList } from "../services/localStorage";
 import { firestore } from "firebase";
 
+let cacheList = [];
 export default function useTodoData() {
   const [todoList, updateTodoList] = useState(INITIAL_STATE());
   const [isLoading, setIsLoading] = useState(false);
@@ -11,49 +12,74 @@ export default function useTodoData() {
 
   const db = firestore();
   const listRef = db.collection("todo");
+
   useEffect(() => {
     startFetching();
-    fetchData
-      .then(list => {
-        storeTodoList(list);
-        updateTodoList(list);
-      })
+    fetchData()
+      .then(updateListWithCache)
       .then(() => setIsLoading(false))
       .catch(handleError);
-  }, [todoList]);
+  }, []);
 
   /**
    * @todo testing individual function
    **/
+
   function INITIAL_STATE() {
     return getTodoList() || [];
   }
-  const fetchData = new Promise(function _fetchData(resolve, reject) {
-    listRef
-      .get()
-      .then(querySnapshot =>
-        querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-      )
-      .then(resolve)
-      .catch(err => reject(err));
-  });
+
+  function fetchData() {
+    return new Promise(function _fetchData(resolve, reject) {
+      listRef
+        .orderBy("createdTime")
+        .get()
+        .then(getListFromSnapshot)
+        .then(resolve)
+        .catch(err => reject(err));
+    });
+  }
+
+  function getListFromSnapshot(snapshot) {
+    return snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+  }
+
   function addTodo(message) {
     startFetching();
-    listRef
-      .add({ done: false, message }) // create new document in firestore collection
+    const newTodoRef = listRef.doc(); // getting the new document id from firestore
+    const newData = {
+      done: false,
+      message,
+      createdTime: firestore.Timestamp.fromDate(new Date()),
+      modifiedTime: firestore.Timestamp.fromDate(new Date())
+    };
+    updateListWithCache(todoList.concat([{ id: newTodoRef.id, ...newData }]));
+    newTodoRef
+      .set(newData) // create new document in firestore collection
       .then(() => setIsLoading(false))
       .catch(handleError);
   }
   function updateTodoCompletion(id, done) {
     startFetching();
+    const newData = {
+      done,
+      modifiedTime: firestore.FieldValue.serverTimestamp()
+    };
+    updateListWithCache(
+      todoList.map(i => (i.id === id ? { ...i, ...newData } : i))
+    );
     listRef
       .doc(id)
-      .update({ done })
+      .update(newData)
       .then(() => setIsLoading(false))
       .catch(handleError);
   }
   function removeTodo(id) {
     startFetching();
+    updateListWithCache(todoList.filter(i => i.id !== id));
     listRef
       .doc(id)
       .delete()
@@ -61,12 +87,21 @@ export default function useTodoData() {
       .catch(handleError);
   }
   function handleError() {
+    undoChanges();
     setIsError(true);
     setIsLoading(false);
   }
   function startFetching() {
+    cacheList = [...todoList];
     setIsError(false);
     setIsLoading(true);
+  }
+  function updateListWithCache(list) {
+    storeTodoList(list);
+    updateTodoList(list);
+  }
+  function undoChanges() {
+    updateListWithCache(cacheList || []);
   }
 
   return {
